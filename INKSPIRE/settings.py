a16133b8ebaf4ee76d780a239f9592a43a75058e
luisdrz20 <luisdavidrs002@gmail.com>
2026-01-15 21:54:24 -0600

    actualizacion grande3

--- INKSPIRE/settings.py
+++ INKSPIRE/settings.py
@@ -95,14 +95,12 @@ WSGI_APPLICATION = 'INKSPIRE.wsgi.application'
 # }
 
 
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
+
+
 
 # DATABASES = {
 #     'default': {
